80c08e49e1dbf4149329a2de4765c58832259805
Sylvan Butler <gitstuff.sylvan@xoxy.net>
2019-06-08 21:54:13 -0600

    restore bitcoin.mnemonic

--- bitcoin/__init__.py
+++ bitcoin/__init__.py
@@ -7,5 +7,4 @@ from bitcoin.bci import *
 from bitcoin.composite import *
 from bitcoin.stealth import *
 from bitcoin.blocks import *
-# Needs wordlists added back in for this to work.
-# from bitcoin.mnemonic import *
+from bitcoin.mnemonic import *
